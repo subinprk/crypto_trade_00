b35fee5b4e7651c50c495540c180dcfccfe23c41
subinprk <subpark@student.42berlin.de>
2024-04-01 23:56:39 +0200

    make them crawl multiple pages

--- crypto_trade/data/count_articles.py
+++ crypto_trade/data/count_articles.py
@@ -1,12 +1,43 @@
-
+import sqlite3
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-#resp = requests.get(BASE_URL, params=params, headers=headers)
-# resp.content
-# soup = BeautifulSoup(resp.content, 'html.parser')
-# contents = soup.find('tbody').find_all('tr')
-# print(contents)
+def putting_data_into_db():
+	conn = sqlite3.connect('dcinside.db')
+	cursor = conn.cursor()
+	cursor.execute('CREATE TABLE IF NOT EXISTS dcinside (title TEXT, writer TEXT, ip TEXT, date TEXT, views TEXT, recommend TEXT)')
+	conn.commit()
+	conn.close()
+
+def process_data(contents):
+	for i in contents:
+		if i == 0 or i == 1: # 0, 1번째 글은 공지사항이므로 제외
+			continue
+		title_tag = i.find('a')
+		title = title_tag.text
+		writer_tag = i.find('td', class_='gall_writer ub-writer').find('span', class_='nickname')
+		if writer_tag is not None: # None 값이 있으므로 조건문을 통해 회피 
+			writer = writer_tag.text
+		else:
+			writer = "Nan"
+		ip_tag = i.find('td', class_='gall_writer ub-writer').find('span', class_='ip')
+		if ip_tag is not None:  # None 값이 있으므로 조건문을 통해 회피 
+			ip = ip_tag.text
+		else:
+			ip = "Nan"
+		date_tag = i.find('td', class_='gall_date')
+		date_dict = date_tag.attrs
+		if len(date_dict) == 2:
+			date = date_dict['title']
+		else:
+			date = date_tag.text
+		views_tag = i.find('td', class_='gall_count')
+		views = views_tag.text
+		recommend_tag = i.find('td', class_='gall_recommend')
+		recommend = recommend_tag.text
+		#print("추천수: ", recommend)
+
 
 
 def crawl_pages(url, num_pages):
@@ -19,53 +50,8 @@ def crawl_pages(url, num_pages):
 			process_data(contents)
 		else:
 			print(f"Failed to fetch page {page}")
-			
 
-def process_data(contents):
-	for i in contents:
-		print('-'*15)
-		
-		# 제목 추출
-		title_tag = i.find('a')
-		title = title_tag.text
-		print("제목: ", title)
-		
-		# 글쓴이 추출
-		writer_tag = i.find('td', class_='gall_writer ub-writer').find('span', class_='nickname')
-		if writer_tag is not None: # None 값이 있으므로 조건문을 통해 회피 
-			writer = writer_tag.text
-			print("글쓴이: ", writer)
-			
-		else:
-			print("글쓴이: ", "없음")
-		
-		# 유동이나 고닉이 아닌 글쓴이 옆에 있는 ip 추출
-		ip_tag = i.find('td', class_='gall_writer ub-writer').find('span', class_='ip')
-		if ip_tag is not None:  # None 값이 있으므로 조건문을 통해 회피 
-			ip = ip_tag.text
-			print("ip: ", ip)
-		
-		# 날짜 추출 
-		date_tag = i.find('td', class_='gall_date')
-		date_dict = date_tag.attrs
 
-		if len(date_dict) == 2:
-			print("날짜: ", date_dict['title'])
-		
-		else:
-			print("날짜: ", date_tag.text)
-			pass
-		
-		# 조회 수 추출
-		views_tag = i.find('td', class_='gall_count')
-		views = views_tag.text
-		print("조회수: ", views)
-		
-		# 추천 수 추출
-		recommend_tag = i.find('td', class_='gall_recommend')
-		recommend = recommend_tag.text
-		print("추천수: ", recommend)
-		
 BASE_URL = "https://gall.dcinside.com/board/lists"
 params={
 	'id': 'bitcoins_new1',
@@ -73,5 +59,5 @@ params={
 headers = {
 	'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0'
 }
-num_pages = 15
+num_pages = 50
 crawl_pages(BASE_URL, num_pages)
